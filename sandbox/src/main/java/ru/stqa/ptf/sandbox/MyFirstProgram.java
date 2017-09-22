d15d8f1981926da366a9b5b7be2d1ad9fc2455d6
Anna <aeyzips@gmail.com>
2017-09-22 14:34:32 -0400

    Classes added

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
@@ -6,12 +6,12 @@ public class MyFirstProgram {
     hello("user");
     hello("Alexei");
 
-    double l = 5;
-    System.out.println("Square " + l + " = " + area(l));
+    Square s = new Square(5);
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Square " + a + " and " + b + " = " + area(a,b));
+    System.out.println("Square " + s.l + " = " + area(s));
+
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Rectangle " + r.a + " and " + r.b + " = " + area(r));
 
   }
   public static void hello(String somebody) {
@@ -19,12 +19,12 @@ public class MyFirstProgram {
 
   }
 
-  public static double area(double len) {
-    return len * len;
+  public static double area(Square s) {
+    return s.l * s.l;
   }
 
-  public static double area(double a, double b) {
-    return a * b;
+  public static double area(Rectangle r) {
+    return r.a * r.b;
 
   }
 }
