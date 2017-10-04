7c60e64155f047d71c9ffe7775f3a72b8448dc59
Anna <aeyzips@gmail.com>
2017-10-04 17:00:02 -0400

    4.8 Searching max value in a collection

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,15 +1,28 @@
 package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
+
+
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
   public GroupData(String name, String header, String footer) {
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
+
+  public GroupData(int id, String name, String header, String footer) {
+    this.id = id;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public int getId() { return id; }
 
   public String getName() {
     return name;
@@ -37,11 +50,20 @@ public class GroupData {
 
     GroupData groupData = (GroupData) o;
 
+    if (id != groupData.id) return false;
     return name != null ? name.equals(groupData.name) : groupData.name == null;
   }
 
   @Override
   public int hashCode() {
-    return name != null ? name.hashCode() : 0;
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
   }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+
 }
